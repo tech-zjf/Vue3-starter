4ecff3f9cc8aa878f550a7d00396c0e6a7ad0c86
tech-zjf <907572438@qq.com>
2023-04-12 15:10:18 +0800

    moment

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 import Api from "./api";
 import "@/assets/style/main.css";
+import moment from "moment";
 
 const app = createApp(App);
 app.config.globalProperties.$Api = Api;
+app.config.globalProperties.$moment = moment;
 
 app.use(createPinia());
 app.use(router);
